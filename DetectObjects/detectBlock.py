c0ac5d2abc6d7559dd913af586f767f51d0960da
dmayo2 <dmayo2@mit.edu>
2016-01-14 16:06:10 -0500

    pid fix

--- DetectObjects/detectBlock.py
+++ DetectObjects/detectBlock.py
@@ -13,12 +13,13 @@ while 1:
 	contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
 	# Find the index of the largest contour
-	areas = [cv2.contourArea(c) for c in contours]
-	max_index = np.argmax(areas)
-	cnt=contours[max_index]
-
-	x,y,w,h = cv2.boundingRect(cnt)
-	cv2.rectangle(im,(x,y),(x+w,y+h),(0,255,0),2)
+	if(len(contours)>0):
+		areas = [cv2.contourArea(c) for c in contours]
+		max_index = np.argmax(areas)
+		cnt=contours[max_index]
+		if(cv2.contourArea(cnt)>=150):
+			x,y,w,h = cv2.boundingRect(cnt)
+			cv2.rectangle(im,(x,y),(x+w,y+h),(0,255,0),2)
 
 	cv2.imshow("Show",im)
 	k = cv2.waitKey(5) & 0xFF

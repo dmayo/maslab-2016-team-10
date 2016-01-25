7b32fb4a6937d9670181015b606baec60be518aa
dmayo2 <dmayo2@mit.edu>
2016-01-25 18:04:43 -0500

    added image processing main

---
+++ otherbot/imageProcessing/test_main2.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <cstring>
+#include <stdlib.h>     /* atoi */
+#include "ImageProcessor.h"
+#include <signal.h>
+#include <sys/time.h>
+#include  <thread>
+#include <cmath>
+
+
+int main( int, char** argv) {
+    ImageProcessor myImageProcessor;
+    //myImageProcessor.run(&myImageProcessor);
+    printf("something worked");
+}
+

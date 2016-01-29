f14baeea240cd271fc88884922dec89de3491028
dmayo2 <dmayo2@mit.edu>
2016-01-29 11:45:07 -0500

    updated arm params

--- otherAttempts/brain/arm.py
+++ otherAttempts/brain/arm.py
@@ -4,8 +4,8 @@ class Arm:
 	def __init__(self,tamp):
 		self.tamp=tamp
 		self.servo = Servo(self.tamp, 9)
-		self.servo.bottom = 0
-		self.servo.top = 200
+		self.servo.bottom = 5
+		self.servo.top = 180
 		self.servo.speed = 30
 		self.servo.write(self.servo.bottom)
 		self.servoval = self.servo.bottom

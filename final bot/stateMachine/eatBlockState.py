676a7aae78bf17f02e354836478243c6f4603e22
Alan Casallas <alancasallas@gmail.com>
2016-01-29 06:13:13 -0500

    updated to include collision info

--- final bot/stateMachine/eatBlockState.py
+++ final bot/stateMachine/eatBlockState.py
@@ -44,10 +44,18 @@ class EatBlockState(state):
 				if self.substate == "EatBlock":
 					self.driveStraight(self.DRIVE_SPEED)
 					if self.isColliding():
+						status_line = 'Collision Info: '
+						for x in xrange(6):
+							status_line +='Sensor '
+							status_line += str(x)
+							status_line += " :"
+							status_line += "{:6.2f}".format(self.sensors.irArray.ir_value[x])
+							status_line += " "
+						print status_line
 						self.driveStraight(0)
 						self.eat_distance -= self.sensors.encoders.getDistanceTraveled()
 						self.substate = "DragBlock"
-					elif self.sensors.encoders.getDistanceTraveled() >= self.EAT_DISTANCE:
+					elif self.sensors.encoders.getDistanceTraveled() >= self.eat_distance:
 						print 'Finished attempt to eat block. Break beam did not go off.'
 						return lookingForBlocksState.LookingForBlocksState(self.sensors, self.actuators, self.motorController, self.timer, self.utils)
 				elif self.substate == "DragBlock":
